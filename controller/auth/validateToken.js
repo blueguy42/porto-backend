bea7daaa71228227b700b92117c10fce6cc86b00
Ahmad Alfani Handoyo <70305222+blueguy42@users.noreply.github.com>
2023-04-13 07:53:58 +0700

    Update validateToken.js

--- controller/auth/validateToken.js
+++ controller/auth/validateToken.js
@@ -6,7 +6,7 @@ async function validateToken(req, res) {
         const accessToken = req.body.token;
 
         const decodedToken = await auth.verifyIdToken(accessToken);
-        res.status(200).json({ msg: 'Token is valid.', email: decodedToken.email });
+        res.status(200).json({ msg: 'Token is valid.', email: decodedToken.email, name: decodedToken.name });
         } catch (error) {
         console.error(error);
         res.status(401).json({ msg: 'Unauthorized. Token is invalid.' });
