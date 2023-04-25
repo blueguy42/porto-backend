f09329ecab9096742a28938ff20734ff494d7f21
Ahmad Alfani Handoyo <70305222+blueguy42@users.noreply.github.com>
2023-04-25 14:29:08 +0700

    forgot that email is already in array format

--- controller/email/update.js
+++ controller/email/update.js
@@ -9,18 +9,6 @@ async function update(req, res) {
             throw new Error(`List of allowed emails does not exist.`);
         }
         const newEmails = req.body.email;
-
-        const validEmails = newEmails.split(', ');
-        let valid = true;
-        validEmails.forEach((email) => {
-            if (!email.match(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g)) {
-                valid = false;
-            }
-        });
-
-        if (!valid) {
-            throw new Error(`Invalid email format.`);
-        }
         
         await docRef.update({ email: newEmails });
         res.status(200).json({ msg: 'Successfully updated list of allowed emails.', email: newEmails });
