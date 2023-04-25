9370e0407021d777b554b8ee99e3cefa764f5778
Ahmad Alfani Handoyo <70305222+blueguy42@users.noreply.github.com>
2023-04-25 14:31:57 +0700

    changed condition for update email

--- controller/email/update.js
+++ controller/email/update.js
@@ -9,6 +9,18 @@ async function update(req, res) {
             throw new Error(`List of allowed emails does not exist.`);
         }
         const newEmails = req.body.email;
+
+        if (!Array.isArray(newEmails)) {
+            throw new Error(`List of emails must be an array.`);
+        }
+        for (let i = 0; i < newEmails.length; i++) {
+            if (typeof newEmails[i] !== 'string') {
+                throw new Error(`List of emails must be an array of strings.`);
+            }
+            if (!newEmails[i].match(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g)) {
+                throw new Error(`List of emails must be an array of valid emails.`);
+            }
+        }
         
         await docRef.update({ email: newEmails });
         res.status(200).json({ msg: 'Successfully updated list of allowed emails.', email: newEmails });
