fa6290452fe0129f8a37c2da8e2bb55de63c2dea
Ahmad Alfani Handoyo <70305222+blueguy42@users.noreply.github.com>
2023-04-26 00:35:25 +0700

    Update readLink.js

--- controller/about/readLink.js
+++ controller/about/readLink.js
@@ -10,7 +10,9 @@ async function readLink(req, res) {
         }
         const email = query.data().email;
         const linkedin = query.data().linkedin;
-        res.status(200).json({ msg: 'Successfully got links.', email, linkedin });
+        const github = query.data().github;
+        const instagram = query.data().instagram;
+        res.status(200).json({ msg: 'Successfully got links.', email, linkedin, github, instagram });
     } catch (err) {
         console.error(err);
         res.status(500).json({ msg: 'Internal server error.', error: err.message });
