58586d1f3a600d9abec9c69640354ac807792e56
Ahmad Alfani Handoyo <70305222+blueguy42@users.noreply.github.com>
2023-04-26 09:57:49 +0700

    add email check in auth

--- controller/auth/validateToken.js
+++ controller/auth/validateToken.js
@@ -1,16 +1,27 @@
 const { firebase } = require('../../utils');
-const { auth } = firebase;
+const { auth, db } = firebase;
 
 async function validateToken(req, res) {
     try {
         const accessToken = req.body.token;
-
         const decodedToken = await auth.verifyIdToken(accessToken);
-        res.status(200).json({ msg: 'Token is valid.', email: decodedToken.email, name: decodedToken.name });
-        } catch (err) {
+
+        const docRef = db.collection('frontend-db').doc("admin");
+        const query = await docRef.get();
+        if (!query.exists) {
+            throw new Error(`List of allowed emails does not exist.`);
+        }
+        const validAdmin = query.data().admin;
+        const validEmails = query.data().email;
+        if (validEmails.includes(decodedToken.email) || validAdmin === decodedToken.email) {
+            return res.status(200).json({ msg: 'Token is valid.', email: decodedToken.email, name: decodedToken.name });
+        } else {
+            throw new Error(`Email is not allowed.`);
+        }
+    } catch (err) {
         console.error(err);
         res.status(401).json({ msg: 'Unauthorized. Token is invalid.', error: err.message });
-        }
+    }
 }
 
 module.exports = validateToken;
--- controller/auth/verifyTokenMiddleware.js
+++ controller/auth/verifyTokenMiddleware.js
@@ -1,11 +1,21 @@
 const { firebase } = require('../../utils');
-const { auth } = firebase;
+const { auth, db } = firebase;
 
 async function verifyTokenMiddleware(req, res, next) {
     try {
         const accessToken = req.headers.authorization.split(' ')[1];
         const decodedToken = await auth.verifyIdToken(accessToken);
-        req.user = decodedToken;
+        
+        const docRef = db.collection('frontend-db').doc("admin");
+        const query = await docRef.get();
+        if (!query.exists) {
+            throw new Error(`List of allowed emails does not exist.`);
+        }
+        const validAdmin = query.data().admin;
+        const validEmails = query.data().email;
+        if (!(validEmails.includes(decodedToken.email) || validAdmin === decodedToken.email)) {
+            throw new Error(`Email is not allowed.`);
+        }
         next();
     } catch (err) {
         res.status(401).json({ msg: 'Unauthorized. Token is invalid.', error: err.message });
