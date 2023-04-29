2d65534362e34504b9844775dd20e3043bb94d09
Ahmad Alfani Handoyo <70305222+blueguy42@users.noreply.github.com>
2023-04-29 10:23:26 +0700

    Update firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,20 +1,4 @@
 const admin = require('firebase-admin');
-const { initializeApp } = require("firebase/app");
-const { getStorage } = require("firebase/storage");
-
-const firebaseConfig = {
-    apiKey: process.env.FIREBASE_CONFIG_APIKEY,
-    authDomain: process.env.FIREBASE_CONFIG_AUTHDOMAIN,
-    projectId: process.env.FIREBASE_CONFIG_PROJECTID,
-    storageBucket: process.env.FIREBASE_CONFIG_STORAGEBUCKET,
-    messagingSenderId: process.env.FIREBASE_CONFIG_MESSAGINGSENDERID,
-    appId: process.env.FIREBASE_CONFIG_APPID,
-    measurementId: process.env.FIREBASE_CONFIG_MEASUREMENTID
-};
-
-const firebaseApp = initializeApp(firebaseConfig);
-
-const storage = getStorage(firebaseApp);
 
 admin.initializeApp({
     credential: admin.credential.cert({
@@ -28,11 +12,14 @@ admin.initializeApp({
         token_uri: process.env.FIREBASE_TOKEN_URI,
         auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
         client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-    })
+    }),
+    storageBucket: process.env.FIREBASE_CONFIG_STORAGEBUCKET
 });
 
 const auth = admin.auth();
 
 const db = admin.firestore();
+
+const storage = admin.storage().bucket();
 
 module.exports = { auth, db, storage };
